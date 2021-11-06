013c2920b621efd2e8641af4f92a77ef93a9f720
Ahmet Bayramoglu <ahmetbayramoglu@Ahmet-MacBook-Pro.local>
2021-11-06 16:52:02 +0300

    this repo created by tes

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nuxt-link to="/mdContent"> MD Content</nuxt-link>
+    <nuxt-link to="/landing"> Landing</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/mdContent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>{{ test.title }}</h1>
+    <!-- <nuxt-content :document="page" /> -->
+   <span> {{ test.glossary.title }}</span>
+    <nuxt-content :document="test.glossary.GlossDiv" />
+  </div>
+</template>
+ß
+
+<script>
+export default {
+  async asyncData({ $content }) {
+    // const page = await $content('hello').fetch()
+    const test = await $content('test').fetch()
+
+    console.log(test)
+    return {
+      test,
+    }
+  },
+}
+</script>
